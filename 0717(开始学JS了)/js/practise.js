97b44840a10849356dc7935a4a205cbdbd6c4edf
zjy888 <309091044@qq.com>
2020-07-18 15:26:15 +0800

    Ver1.01

---
+++ 0717(开始学JS了)/js/practise.js
@@ -0,0 +1,46 @@
+alert('whatdoyouwantforme');
+
+// 数组 Array
+var a = [18, 24, 16, 22, 17];
+
+console.log(a);
+
+// 对象 Object
+
+var b = {
+    name: '沪指',
+    open: 3214.40,
+    dayRange: '3181.27-3252.78',
+    volume: '356.1亿'
+};
+
+console.log(b);
+
+// var q = 100
+
+// console.log(--q);
+
+// q -= 10
+
+// console.log(q)
+
+// 复杂数据
+var list = [{
+        name: '沪指',
+        open: 3214.40,
+        dayRange: '3181.27-3252.78',
+        volume: '356.1亿'
+    },
+    {
+        name: '深指',
+        open: 13002.84,
+        dayRange: '12916.08-13315.44',
+        volume: '4.77亿'
+    }
+]
+console.log(list)
+
+// 函数
+function say() {
+    
+}
